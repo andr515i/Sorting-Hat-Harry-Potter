e04f4614d131f9e54b9e1422de482056a7946ec8
Andreas Sass Borggaard Madsen <andr515i@zbc.dk>
2022-08-31 14:39:02 +0200

    sorry Kris

---
+++ Sorting Hat Harry Potter/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sorting_Hat_Harry_Potter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // sorting hat program fun
+            // i will only evaluate the user on 1 trait and figure out where to send them from there
+            // traits in question is - bravery, loyal, knowledge-seeking and powerhungry each corresponding to their houses, in that specific order
+
+            SortingHat sh = new SortingHat();  
+
+            bool run = true;
+            while (run)  // while loop to test all cases 
+            {
+                Console.WriteLine("would you describe yourself as mostly brave, loyal, knowledge-seeking or power-hungry?");
+                
+                Console.WriteLine(sh.Sorting(Console.ReadLine())); // the sh.sorting method returns the answer in a string, thefore we just throw everything together and now it looks like im a good or something... just kidding
+
+            }
+            
+        }
+    }
+}
---
+++ Sorting Hat Harry Potter/SortingHat.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sorting_Hat_Harry_Potter
+{
+    class SortingHat
+    {   // I am to make a sorting hat (from harry potter), and to supposedly just give the user a house. 
+        // i will say, due to unspecification of requirements on this task, i.e there is not specified whether or not to have any reason to send kids to specific houses
+        // i am going not going to spend 10 hours on this assignement, even if it is supposed to work my skills in predicting the uncertainty in the missing information (in this case requirements)
+
+        private string sorted;
+        public enum hogwarts
+        {
+            Gryffindor,
+            Hufflepuff,
+            Ravenclaw,
+            Slitherin
+        }
+
+
+        public string Sorting(string trait)  // we make a sorting method to figure out where to send kids dependant on their answer of their trait
+        {
+            switch (trait)
+            {
+                case "brave":
+                    sorted = hogwarts.Gryffindor.ToString();
+                    break;
+                case "loyal":
+                    sorted = hogwarts.Hufflepuff.ToString();
+                    break;
+                case "knowledge-seeking":
+                    sorted = hogwarts.Ravenclaw.ToString();
+                    break;
+                case "power-hungry":
+                    sorted = hogwarts.Slitherin.ToString();
+                    break;
+                default:
+                    sorted = "your answer is case sensitive, please try again";
+                    break;
+            }
+
+            return sorted;
+        }
+
+        
+    }
+}
